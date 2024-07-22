3a00632a3e5482b8e079137dbf6b131e85f23b47
oiviana <vianalucas80@gmail.com>
2024-07-22 15:17:59 -0300

    style: adjust institutional cards mobile

--- components/ui/InstitucionalCards.tsx
+++ components/ui/InstitucionalCards.tsx
@@ -13,10 +13,10 @@ export interface Props {
 export default function InstitucionalCards(cards: Props) {
   return (
     <section class="container mt-4">
-      <div class="grid grid-cols-3 gap-5">
+      <div class="lg:grid grid-cols-3 gap-5">
         {cards && cards.cards.map((card) => {
           return (
-            <div class="card border p-[45px] rounded-[22px] border-solid border-[#E9E9E9]">
+            <div class="card border p-[45px] rounded-[22px] border-solid border-[#E9E9E9] mb-4">
               <h3 class="mb-6 font-semibold text-[30px] leading-9">
                 {card.title}
               </h3>
