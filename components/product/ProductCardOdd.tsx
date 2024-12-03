11621face123565484b9a8bb981e53ca1ef5f4a7
Karla Oshikawa <karla_hara@hotmail.com>
2024-12-04 07:35:08 +0900

    feat: promotion tag ajust

--- components/product/ProductCardOdd.tsx
+++ components/product/ProductCardOdd.tsx
@@ -61,11 +61,10 @@ const relative = (url: string) => {
 
 const calculate = (item: number, item2: number) => {
   if ((item - item2) > 0) {
-    // const percentValue = Math.round((item - item2) / item * 100);
-    // return `${percentValue}% OFF`;
+    const percentValue = Math.round((item - item2) / item * 100);
+    return `${percentValue}% OFF`;
   }
-  //este valor esta colocado diretamente no codigo por causa de um erro na API da wake. Retirar e descomentar o codigo acima para calcular automaticamente.
-  return "12% OFF"
+  
 };
 
 
--- components/search/Filters.tsx
+++ components/search/Filters.tsx
@@ -8,10 +8,14 @@ import type {
 } from "apps/commerce/types.ts";
 import { parseRange } from "apps/commerce/utils/filters.ts";
 import ClearFilters from "site/islands/ClearFilters.tsx";
-// import FilterRange from "site/components/search/FilterRange.tsx";
+import RangeFilter from "site/islands/RangeFilter.tsx";
 
 interface Props {
   filters: ProductListingPage["filters"];
+  /** @description O valor minimo esta definido em R$0,00 se quiser mudar o valor preencher o campo abaixo */
+  min?: number;
+  /** @description O valor maximo esta definido em R$10.000,00 se quiser mudar o valor preencher o campo abaixo */
+  max?: number;
 }
 
 const isToggle = (filter: Filter): filter is FilterToggle =>
@@ -19,7 +23,10 @@ const isToggle = (filter: Filter): filter is FilterToggle =>
 
 function ValueItem(
   { url, selected, label, quantity }: FilterToggleValue,
+  
+  
 ) {
+console.log(url)
   return (
     <>
       {quantity > 0 && (
@@ -79,7 +86,8 @@ function FilterValues({ key, values }: FilterToggle) {
   );
 }
 
-function Filters({ filters }: Props) {
+function Filters({ filters,min, max }: Props) {
+  
   return (
     <ul class="flex flex-col py-4">
       <div class="collapse collapse-plus collapse-open">
@@ -88,33 +96,31 @@ function Filters({ filters }: Props) {
           SELECIONADOS
         </div>
         <div class="collapse-content">
-          {filters.filter(isToggle)
-            .map((filter) => (
-              filter.values.map((item) => (
-                item.selected && <FilterValues {...filter} />
-              ))
-            ))}
+          {filters
+            .filter(isToggle)
+            .map((filter) =>
+              filter.values.map(
+                (item) => item.selected && <FilterValues {...filter} />
+              )
+            )}
           <ClearFilters />
         </div>
       </div>
 
-      {filters
-        .filter(isToggle)
-        .map((filter) => (
-          <div class="collapse collapse-plus">
-            <input type="checkbox" />
-            <div class="mb-2.5 rounded-none border-b-[#e9e9e9] border-b border-solid collapse-title font-semibold text-[18px] leading-9 after:!w-[30px] after:!h-[30px] after:!flex after:!items-center after:!justify-center after:rounded-md after:border after:border-solid after:border-[#164195]">
-              {filter.label}
-            </div>
-            <div class="collapse-content">
-              <FilterValues {...filter} />
-            </div>
+      {filters.filter(isToggle).map((filter) => (
+        filter.label !== "Preço" ?
+        (<div class="collapse collapse-plus">
+          <input type="checkbox" />
+          <div class="mb-2.5 rounded-none border-b-[#e9e9e9] border-b border-solid collapse-title font-semibold text-[18px] leading-9 after:!w-[30px] after:!h-[30px] after:!flex after:!items-center after:!justify-center after:rounded-md after:border after:border-solid after:border-[#164195]">
+            {filter.label}
           </div>
-          // <li class="flex flex-col gap-4">
-          //   <span>{filter.label}</span>
-          //   <FilterValues {...filter} />
-          // </li>
-        ))}
+          <div class="collapse-content">
+            <FilterValues {...filter} />
+          </div>
+        </div>) : null
+      ))}
+    
+      <RangeFilter min={min} max={ max} />
     </ul>
   );
 }
