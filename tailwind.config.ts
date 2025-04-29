d6d086119fcea8be93ce7bf019357553012a1a80
Danilo Araujo <danilo.araujo@dna360.ag>
2025-04-29 13:17:23 +0000

    Correção aplicada para garantir que os elementos do layout respeitem as cores configuradas no tema. Ajustamos a configuração do Tailwind CSS e do DaisyUI para definir as cores primary e neutral como branco (#ffffff). Além disso, revisamos as classes CSS utilizadas nos elementos do header e dos produtos para evitar conflitos e garantir consistência visual no ambiente do totem. Essa alteração resolve o problema de cores incorretas exibidas no layout.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,18 @@ import daisyui from "daisyui";
 
 export default {
   plugins: [daisyui],
-  daisyui: { themes: [], logs: false },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#ffffff", // Branco
+          neutral: "#ffffff", // Branco
+          "base-100": "#ffffff", // Fundo branco
+        },
+      },
+    ],
+    logs: false,
+  },
   content: ["./**/*.tsx"],
   theme: { container: { center: true } },
 };
