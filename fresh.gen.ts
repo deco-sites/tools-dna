b788ac39f0772e30a21ef5389e9c61798f174b9b
danilodev7 <danilodev7@gmail.com>
2025-06-02 15:22:52 -0300

    🐛 :bug: fix: remover quebra de linha antes da declaração XML no sitemap.xml

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,6 +3,7 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $_app from "./routes/_app.tsx";
+import * as $sitemap_xml from "./routes/sitemap.xml.ts";
 import * as $AddToCartButton_linx from "./islands/AddToCartButton/linx.tsx";
 import * as $AddToCartButton_nuvemshop from "./islands/AddToCartButton/nuvemshop.tsx";
 import * as $AddToCartButton_shopify from "./islands/AddToCartButton/shopify.tsx";
@@ -40,6 +41,7 @@ import type { Manifest } from "$fresh/server.ts";
 const manifest = {
   routes: {
     "./routes/_app.tsx": $_app,
+    "./routes/sitemap.xml.ts": $sitemap_xml,
   },
   islands: {
     "./islands/AddToCartButton/linx.tsx": $AddToCartButton_linx,
